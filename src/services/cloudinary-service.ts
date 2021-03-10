78913ab2ad492d5962267fb89227f0fd9aa85b48
Lelberto <jsurieux@gmail.com>
2021-03-10 10:59:32 +0100

    Add cloudinary service (WIP)

---
+++ src/services/cloudinary-service.ts
@@ -0,0 +1,9 @@
+import Service from './service';
+import ServiceContainer from './service-container';
+
+export default class CloudinaryService extends Service {
+
+  public constructor(container: ServiceContainer) {
+    super(container);
+  }
+}
